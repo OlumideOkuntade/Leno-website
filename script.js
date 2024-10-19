92c6ec5a1e3e15c83d48b55a6a79e72250cd5fbe
Olumide Okuntade <Olumide.okuntade@gmail.com>
2024-10-19 22:15:59 +0100

    deploying

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// mobile menu
 document.addEventListener('DOMContentLoaded',function(){
     const toggleButton = document.querySelector('.navbar__mobile-menu-toggle');
     const mobileMenu = document.querySelector('.navbar__mobile-menu-items');
@@ -6,4 +7,31 @@ document.addEventListener('DOMContentLoaded',function(){
         mobileMenu.classList.toggle('active')
     });
 });
+// navigation scroll
+const navbar = document.querySelector('.navbar');
+    window.addEventListener('scroll', function(){
+    if(window.scrollY > 0){
+        navbar.classList.add('navbar--scroll');
+    }else{
+        navbar.classList.remove('navbar--scroll'); 
+    }
+})
+// Video model
+const modal = document.querySelector('#videoModal');
+const videoButton = document.querySelector('.preview__video-button');
+const closeButton = document.querySelector('.modal__close-button');
+const videoPlayer = document.querySelector('#videoPlayer');
+videoButton.addEventListener('click',function(){
+modal.style.display =' block'
+videoPlayer.src ='';
+});
+closeButton.addEventListener('click',function(){
+modal.style.display ='none'});
+window.addEventListener('click',function(event){
+   if(event.target == modal){
+        modal.style.display ='none'}
+});
 
+    
+
+ 
